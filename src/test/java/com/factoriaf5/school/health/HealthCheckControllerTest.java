6a809a0b672e022df9a85410a4d43d55d5739d06
Sergi Virgili <svirgilif@gmail.com>
2024-07-14 16:43:18 +0200

    Create Health Check (#3)
    * feat: add health-check controller endpoint
    
    * test: add health-check http request
    
    * test: add health-check integration test

---
+++ src/test/java/com/factoriaf5/school/health/HealthCheckControllerTest.java
@@ -0,0 +1,28 @@
+package com.factoriaf5.school.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void healthCheck() throws Exception {
+        mockMvc.perform(get("/api/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("OK"));
+
+    }
+}
