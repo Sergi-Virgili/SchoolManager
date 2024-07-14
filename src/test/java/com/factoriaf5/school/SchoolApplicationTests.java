da600ba682c0c754273d4516f28bb926bd82dfaa
Sergi Virgili <svirgilif@gmail.com>
2024-07-14 16:18:34 +0200

    chore: project initialized (close #1)

---
+++ src/test/java/com/factoriaf5/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
